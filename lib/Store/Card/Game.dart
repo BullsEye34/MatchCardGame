4b125f551fffc8a6686be59767f779f54dd99cdc
Vamshi Prasad <vamvijkum@gmail.com>
2020-06-01 23:36:37 +0530

    Reduce Even More

--- lib/Store/Card/Game.dart
+++ lib/Store/Card/Game.dart
@@ -8,20 +8,12 @@ abstract class _Game with Store {
   @observable
   var card = [
     's2',
-    's3',
-    's4',
     'sA',
     'h2',
-    'h3',
-    'h4',
     'hA',
     'd2',
-    'd3',
-    'd4',
     'dA',
     'c2',
-    'c3',
-    'c4',
     'cA',
   ];
   var reveal = true;
