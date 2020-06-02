c5b7a1895a29c566624f50accc3944bebe78ca53
Vamshi Prasad <vamvijkum@gmail.com>
2020-06-02 20:17:01 +0530

    Constructor

--- lib/Pages/Card.dart
+++ lib/Pages/Card.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Card extends StatefulWidget {
+  var shape;
+  Card(this.shape);
   @override
-  _CardState createState() => _CardState();
+  _CardState createState() => _CardState(shape);
 }
 
 class _CardState extends State<Card> {
+  var shape;
+  _CardState(this.shape);
   @override
   Widget build(BuildContext context) {
     return Container();
