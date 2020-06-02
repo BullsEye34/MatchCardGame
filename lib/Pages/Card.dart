e79fd48c361d13418bbbf379cd740fe8475ee5a3
Vamshi Prasad <vamvijkum@gmail.com>
2020-06-02 20:15:54 +0530

    Make Card Class

---
+++ lib/Pages/Card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
