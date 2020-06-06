537128cf0d327d683edfb3b6fee2416fd636582c
Vamshi Prasad <vamvijkum@gmail.com>
2020-06-06 15:23:53 +0530

    Comment Animation

--- lib/Pages/Game_Page.dart
+++ lib/Pages/Game_Page.dart
@@ -34,11 +34,11 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
   var buffern = -999;
   var yaay = "IDK";
   turn(index) {
-    if (flipStatus == AnimationStatus.dismissed) {
+    /* if (flipStatus == AnimationStatus.dismissed) {
       flipController.forward();
     } else {
       flipController.reverse();
-    }
+    } */
     setState(() {
       newCards[index]["reveal"] = !newCards[index]["reveal"];
     });
